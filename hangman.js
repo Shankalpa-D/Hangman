b522c975ff6d2235f462462f0b1511b79ce564f5
Shankalpa Duwadi <duwadishankalpa@gmail.com>
2025-10-15 10:42:21 -0400

    code to choose random word form list

---
+++ hangman.js
@@ -0,0 +1,12 @@
+var POSSIBLE_WORDS = [
+  "obdurate",
+  "verisimilitude",
+  "defenstrate",
+  "obsequious",
+  "dissonate",
+  "toady",
+  "idempotent",
+];
+
+var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
+var word = POSSIBLE_WORDS[randomIndex];

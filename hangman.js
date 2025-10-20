dbefa08818c654bb1666ef1d9aef27e1f3fcf569
Shankalpa Duwadi <duwadishankalpa@gmail.com>
2025-10-20 10:33:58 -0400

    added guessl lettter funcitos

--- hangman.js
+++ hangman.js
@@ -8,5 +8,33 @@ var POSSIBLE_WORDS = [
   "idempotent",
 ];
 
-var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
-var word = POSSIBLE_WORDS[randomIndex];
+var word = "";
+var guess = "";
+
+function newGame() {
+  var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
+  word = POSSIBLE_WORDS[randomIndex];
+  guesses = "";
+
+  updatepage();
+}
+
+function guessLetter() {
+  var input = document.getElementById("guess");
+  var letter = input.value;
+  guesses += letter;
+
+  updatepage();
+}
+
+function updatepage() {
+  var clueString = "";
+  for (var i = 0; i < word.length; i++) {
+    var currentLetter = word.charAt(i);
+    if (guesses.indexOf(currentLetter) >= 0) {
+      clueString += currentLetter + " ";
+    } else clueString += "_ ";
+  }
+  var clue = document.getElementById("clue");
+  clue.innerHTML = clueString;
+}
